78e3e5b84a66f0880d7040e8ba04fa54e02c96f3
Drew McPherson <drewmac1971@gmail.com>
2019-01-09 20:35:23 -0600

    added variables exercises

---
+++ variables/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // int num1 = 0;
+            // int num2 = 0;
+            // int result = 0;
+
+           // Console.WriteLine("Hello World!");
+          
+          //sum program
+		//  Console.WriteLine("Please enter a number from 1 to 100");
+        //  num1 = Convert.ToInt32(Console.ReadLine());
+        //  Console.WriteLine("Please enter another number from 1 to 100");
+        //  num2 = Convert.ToInt32(Console.ReadLine());
+        //  result = num1 + num2;
+        //  System.Console.WriteLine("The sum of the numbers you entered is {0}", result);
+
+         //convert yards to inches
+         int yards = 3;
+         
+          float result = 0;
+
+          //Console.WriteLine("Please enter a number for yards");
+          //yards = Convert.ToInt32(Console.ReadLine());
+          result = yards * 36;
+          System.Console.WriteLine("The number of inches in {0} yards is {1}", yards, result);
+
+        bool people = true;
+        bool f = false;
+        decimal num = 1.5;
+
+        System.Console.WriteLine("the product of num times itself is {0}", num*num);
+
+
+
+
+        }
+    }
+}
