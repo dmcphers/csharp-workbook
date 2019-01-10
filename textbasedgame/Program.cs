c275613d8877bf0771e7b74bfe53fc64a61c9a55
Drew McPherson <drewmac1971@gmail.com>
2019-01-09 21:07:44 -0600

    first commit of textbasedgame

---
+++ textbasedgame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace textbasedgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ch1;
+            System.Console.WriteLine("You enter a dark cavern out of curiosity. It is dark and you can only make out a small stick on the floor. Do you take it? [y/n]");
+            //ch1 = str(input("Do you take it? [y/n]: "))
+            ch1 = Console.ReadLine();
+
+            if (ch1 == "y")
+            {
+                System.Console.WriteLine("You have chosen the stick!");
+            }
+        }
+
+    }
+}
