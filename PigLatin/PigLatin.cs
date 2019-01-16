e6b647d3b5f2ed5bfd310a8a03aa5e472de2feae
Drew McPherson <drewmac1971@gmail.com>
2019-01-15 19:51:00 -0600

    first commit of piglatin project

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -7,9 +7,17 @@ namespace PigLatin
         public static void Main()
         {
             // your code goes here
+            string word = Console.ReadLine();
+            string firstLetter = word.Substring(0,1);
+            int firstLetterIndex = word.IndexOf(firstLetter);
+            //Console.WriteLine(firstLetterIndex);
+            string restWord = word.Substring(firstLetterIndex + 1);
+            Console.WriteLine(restWord + firstLetter + "ay");
 
+
+            //Console.WriteLine(restWord);
             // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
+            //Console.ReadLine();
         }
         
         public static string TranslateWord(string word)
