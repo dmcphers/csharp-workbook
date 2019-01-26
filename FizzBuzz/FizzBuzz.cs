8819e03dff59b45f03c7ba30d6aebc560194953f
Drew McPherson <drewmac1971@gmail.com>
2019-01-26 14:00:27 -0600

    first commit of fizzbuzz class project

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,27 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number between 1 and 100 for this fizzbuzz program to use:");
+            int userNumber = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= userNumber; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) 
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
---
+++ sort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sort
+{
+    class Program
+    {
+        int[] numbers = {20, 10, 50, 80, 15};
+        static void Main(int[] numbers)
+        {
+            //Console.WriteLine("Hello World!");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                 if (numbers[i] > numbers[i + 1])
+                 {
+                     numbers[i + 1] = numbers[0];
+                     
+                 }
+            }
+            
+        }
+    }
+}
