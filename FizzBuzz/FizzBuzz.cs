ef35310e833f51fe93965c490b609a0996981477
Drew McPherson <drewmac1971@gmail.com>
2019-01-31 21:22:37 -0600

    committed fizzbuzz and sort projects for class

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,8 +6,13 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+
+            // get input from user
             Console.WriteLine("Enter a number between 1 and 100 for this fizzbuzz program to use:");
+            // convert user input to integer type and store it in a variable
             int userNumber = Int32.Parse(Console.ReadLine());
+            // loop through each number from 1 to user number and give appropriate output based on whether
+            // the user number is divisible by 3 or 5 or both
             for (int i = 1; i <= userNumber; i++)
             {
                 if (i % 3 == 0 && i % 5 == 0) 
--- sort/Program.cs
+++ sort/Program.cs
@@ -6,32 +6,41 @@ namespace sort
     {
         static void Main()
         {
+            // set sort to false and initialize array with bunch of unsorted numbers
             bool sort = false;
-            int[] numbers = new int[]{3434,1242,8595,2942,293473,92343,987434,46374};
-            
+            int[] numbers = new int[]{25,75,12,24,48,88,344,3,159,77};
+                    // loop through array
                     for (int i = 0; i < numbers.Length - 1; i++)
                     {
+                        // set current position in array to variable current and next position in array to variable next
                         int current = numbers[i];
                         int next = numbers[i + 1];
 
+                        // if number in current index is greater than or equal to next position, then
+                        // set current to next and next to current (switch them), and set sort to false
                         if (numbers[i] >= numbers[i + 1])
                         {
                            numbers[i] = next;
                            numbers[i + 1] = current;
                            sort = false;
                         }
+                        // if number in current index is less than number of next position, then set 
+                        // current index number to the variable current and set sort to true
                         else if (numbers[i] < numbers[i + 1])
                         {
                             numbers[i] = current;
                             sort = true;
                         }
+                        // if sort is not equal to true, then set counter to -1 so that for loop will begin
+                        // at index 0 again
                             if (!sort)
                             {
-                                i = 0;
+                                i = -1;
                             }
                         
                     }
-         
+            
+            // print out to the console the sorted numbers array
             for (int i = 0; i < numbers.Length; i++)
             {
                  System.Console.WriteLine(numbers[i]);
