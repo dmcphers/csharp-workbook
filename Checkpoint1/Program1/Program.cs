e6d6f964ab4ac529e5d9d9e781ab8fb1098a90b2
Drew McPherson <drewmac1971@gmail.com>
2019-02-02 21:20:12 -0600

    first commit of checkpoint 1

---
+++ Checkpoint1/Program1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // ask user question that will determine output of program
+            Console.WriteLine("Would you like to see how many numbers between 1 and 100 are divisible by 3 ?");
+            Console.WriteLine("If yes press y, if no press n");
+
+            // read in user input and store it in userInput variable
+             string userInput = Console.ReadLine().ToLower();
+
+             // pass in userInput value to outputResult function
+             int screenOutput = outputResult(userInput);
+
+
+            // user condition to output message to screen based on user input
+             if (screenOutput == 1)
+             {
+                 // call function to calculate answer and store answer in answer variable
+                int answer = getAnswer();
+                Console.WriteLine($"There are {answer} numbers between 1 and 100 that are divisible by 3 !");
+             } 
+             else if (screenOutput == 2)
+             {
+                Console.WriteLine("Ok, well if you ever want to in the future, come back and run this program again.");
+             } 
+             else 
+             {
+                 Console.WriteLine("You didn't enter a y or an n, please run the program again");
+             }
+        }
+        // function recieves userInput and returns number that will determine whether to continue program or not
+        static int outputResult(string userInput)
+            {
+                if (userInput == "y")
+                {
+                    return 1;
+                } else if (userInput == "n")
+                {
+                    return 2;
+                } else
+                {
+                    return 0;
+                }
+            }
+            // funtion calculates the answer to how many numbers are divisible between 1 and 100 by 3
+        static int getAnswer()
+            {
+                int count = 0;
+                for (int i = 1; i < 100; i++)
+                {
+                    if (i%3 == 0)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+    }
+}
---
+++ Checkpoint1/Program2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // inform user of purpose of program
+            Console.WriteLine("This program will take all the numbers you enter and add them together.");
+            
+            // initialize a list to hold numbers user enters
+            List<int> numbersList = new List<int>{};
+            // declare and initialize a count variable that will be used to keep track if user has entered any numbers
+            int count = 0;
+            // declare and initialize sum variable to hold sum of all values user enters
+            int sum = 0;
+            // declare and initialize continueInput variable to determine whether program continues or stops
+            bool continueInput = false;
+
+        do {
+                // give user instructions
+                Console.WriteLine("Please enter either a number or the word 'ok' to exit.");
+                // read input from user
+                string userStringInput = Console.ReadLine();
+                // declare an integer variable
+                int userNumberInput;
+                // see if user input can be converted to a number and set true or false value into isNumber variable
+                bool isNumber = int.TryParse(userStringInput, out userNumberInput);
+            
+                // if user input is a number
+                if (isNumber)
+                    {
+                        // add user number to the numbers list
+                        numbersList.Add(userNumberInput);
+                        // calculate sum of user numbers
+                        sum = sum + userNumberInput;
+                        // increment the count of user entries
+                        count++;
+                        // set continueInput value to true to allow for continuation of program
+                        continueInput = true;
+                    } 
+
+                // if user enters 'ok'
+                else if (userStringInput == "ok")
+                {
+                    // if this is user's first entry, give message that no numbers were entered and set contineInput
+                    // to false to end program
+                    if (count == 0)
+                    {
+                        Console.WriteLine("No numbers were entered.");
+                        continueInput = false;
+                    }
+                    else
+                    // display output of numbers user entered, the sum of the numbers entered, and
+                    // set continueInput to false to end program
+                    {
+                        System.Console.WriteLine("The numbers you entered were: ");
+                        foreach (int number in numbersList)
+                        {
+                            System.Console.WriteLine(number);
+                        }
+                        System.Console.WriteLine("The sum of the numbers you entered is: {0}", sum);
+                        continueInput = false;
+                    }
+                    
+                } 
+
+            } while (continueInput);
+        }
+    }
+}
