d7bbff082010a18631c0b10334f6a7adf3d830f0
Drew McPherson <drewmac1971@gmail.com>
2019-02-02 23:35:47 -0600

    added program 5 to checkpoint 1 - not finished yet

---
+++ Checkpoint1/Program5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace program5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inform user what the program will do
+            Console.WriteLine("This program will recieve a comma separated series of numbers that you enter.");
+            System.Console.WriteLine("The program will then output the greatest number of the numbers you entered.");
+            // prompt user to enter a number
+            System.Console.WriteLine("Please enter a series of numbers separated by commas.");
+            // read the user input into a variable
+            string userStringInput = Console.ReadLine();
+        }
+    }
+}
