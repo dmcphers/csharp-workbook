135cbfd7f8c2fdf256fa24f6d995cb928d5137ba
Drew McPherson <drewmac1971@gmail.com>
2019-02-03 20:36:45 -0600

    simplified program1 of checkpoint 1 even further

--- Checkpoint1/Program1/Program.cs
+++ Checkpoint1/Program1/Program.cs
@@ -7,48 +7,12 @@ namespace Program1
         
         static void Main(string[] args)
         {
-
-            // ask user question that will determine output of program
-            Console.WriteLine("Would you like to see how many numbers between 1 and 100 are divisible by 3 ?");
-            Console.WriteLine("If yes press y, if no press n");
-
-            // read in user input and store it in userInput variable
-             string userInput = Console.ReadLine().ToLower();
-
-             // pass in userInput value to outputResult function
-             int screenOutput = outputResult(userInput);
-
-
-            // user condition to output message to screen based on user input
-             if (screenOutput == 1)
-             {
-                 // call function to calculate answer and store answer in answer variable
-                int answer = getAnswer();
-                Console.WriteLine($"There are {answer} numbers between 1 and 100 that are divisible by 3 !");
-             } 
-             else if (screenOutput == 2)
-             {
-                Console.WriteLine("Ok, well if you ever want to in the future, come back and run this program again.");
-             } 
-             else 
-             {
-                 Console.WriteLine("You didn't enter a y or an n, please run the program again");
-             }
+            // call getAnswer function which calculates all numbers between 1 and 100 that are divisible by 3
+            // and place the return value of that function in the variable answer
+            int answer = getAnswer();
+            // output message stating how many numbers between 1 and 100 are divisible by 3
+            System.Console.WriteLine("There are {0} numbers between 1 and 100 that are divisible by 3.", answer);
         }
-        // function recieves userInput and returns number that will determine whether to continue program or not
-        static int outputResult(string userInput)
-            {
-                if (userInput == "y")
-                {
-                    return 1;
-                } else if (userInput == "n")
-                {
-                    return 2;
-                } else
-                {
-                    return 0;
-                }
-            }
             // funtion calculates the answer to how many numbers are divisible between 1 and 100 by 3
         static int getAnswer()
             {
