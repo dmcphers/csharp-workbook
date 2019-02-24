1d3ea842aa374dc46f92d9c6a077559ee634d713
Drew McPherson <drewmac1971@gmail.com>
2019-02-23 21:43:30 -0600

    created a foler and program to test enums

---
+++ enums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please enter your birthday in format of yyyy, mm, dd:");
+            string strDate = Console.ReadLine();
+            DateTime dt = new DateTime();
+            dt = DateTime.Parse(strDate);
+            //DateTime dt = new DateTime(2019, 2, 19);
+            //System.Console.WriteLine("Your birtday is {0}", dt);
+            System.Console.WriteLine("The day of the week of your birthday was {0}", dt.DayOfWeek);
+        }
+    }
+}
