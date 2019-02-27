a1cc236096d18fecebe5358be1ba3b2702b6535f
Drew McPherson <drewmac1971@gmail.com>
2019-02-26 20:18:32 -0600

    finished enum, struct, and linq exercises for class

--- enums/Program.cs
+++ enums/Program.cs
@@ -6,31 +6,25 @@ namespace enums
     {
         static void Main(string[] args)
         {
-            // System.Console.WriteLine("Please enter your birthday in format of yyyy, mm, dd:");
-            // string strDate = Console.ReadLine();
-            // DateTime dt = new DateTime();
-            // dt = DateTime.Parse(strDate);
-            // //DateTime dt = new DateTime(2019, 2, 19);
-            // //System.Console.WriteLine("Your birtday is {0}", dt);
-            // System.Console.WriteLine("The day of the week of your birthday for the year {0} is {1}",dt.Year, dt.DayOfWeek);
-            System.Console.WriteLine(DaysOfWeek.Monday);
-            System.Console.WriteLine(DaysOfWeek.Tuesday);
-            System.Console.WriteLine(DaysOfWeek.Wednesday);
-            System.Console.WriteLine(DaysOfWeek.Thursday);
-            System.Console.WriteLine(DaysOfWeek.Friday);
-            System.Console.WriteLine(DaysOfWeek.Saturday);
-            System.Console.WriteLine(DaysOfWeek.Sunday);
-            System.Console.WriteLine();
+            // System.Console.WriteLine(DaysOfWeek.Monday);
+            // System.Console.WriteLine(DaysOfWeek.Tuesday);
+            // System.Console.WriteLine(DaysOfWeek.Wednesday);
+            // System.Console.WriteLine(DaysOfWeek.Thursday);
+            // System.Console.WriteLine(DaysOfWeek.Friday);
+            // System.Console.WriteLine(DaysOfWeek.Saturday);
+            // System.Console.WriteLine(DaysOfWeek.Sunday);
+            // System.Console.WriteLine();
 
             
-             foreach (var item in Enum.GetValues(typeof(System.DayOfWeek)))
+            // foreach (var item in Enum.GetValues(typeof(System.DayOfWeek)))
+             foreach (var item in Enum.GetValues(typeof(DaysOfWeek)))
             {
-                var monthEnum = (DayOfWeek)item;
+                var monthEnum = (DaysOfWeek)item;
                 System.Console.WriteLine(monthEnum.ToString());
             }
-    }
-
         }
+
+    
         enum DaysOfWeek
             {
                 Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
@@ -46,10 +40,6 @@ namespace enums
             }
           public int X { get; set;}
           public int Y { get; set;}
-          public static void CalculateValue()
-          {
-              
-          }
         }
-
+    }
 }
