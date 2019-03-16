4ea0efee4e1ea166666bdd3403b0cfa9359a402c
Drew McPherson <drewmac1971@gmail.com>
2019-03-15 21:54:23 -0500

    final commit for checkpoint 3 for class

---
+++ EF/EF/EF/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
---
+++ ToDoApp/ToDoApp/ToDoApp/ToDoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp
+{
+    public class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
